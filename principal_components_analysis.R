4cf8f4375f25f5b348cc3f36206e66b783de8ad3
fverkroost <12556240+fverkroost@users.noreply.github.com>
2020-10-15 12:41:08 +0100

    Update principal_components_analysis.R

--- principal_components_analysis.R
+++ principal_components_analysis.R
@@ -7,8 +7,6 @@
 #                                       #
 #########################################
 
-# Note: Function "double_axis_graph" was obtained from http://stackoverflow.com/questions/25727596/function-to-build-double-y-axis-graph-in-ggplot2.
-
 library(ggplot2) # load ggplot2 package into R
 library(stats) # load ggplot2 package into R
 library(ggrepel) # load ggplot2 package into R
@@ -87,29 +85,6 @@ biplot2 <- biplot2 + geom_hline(aes(yintercept=0), size=.2) + geom_vline(aes(xin
   theme_bw() + theme(panel.grid=element_blank()) +
   theme(panel.background = element_rect(fill = NA))
 
-# Function double_axis_graph via http://stackoverflow.com/questions/25727596/function-to-build-double-y-axis-graph-in-ggplot2
-double_axis_graph <- function(graf1,graf2){
-  graf1 <- graf1
-  graf2 <- graf2
-  gtable1 <- ggplot_gtable(ggplot_build(graf1))
-  gtable2 <- ggplot_gtable(ggplot_build(graf2))
-  par <- c(subset(gtable1[['layout']], name=='panel', select=t:r))
-  graf <- gtable_add_grob(gtable1, gtable2[['grobs']][[which(gtable2[['layout']][['name']]=='panel')]],
-                          par['t'],par['l'],par['b'],par['r'])
-  ia <- which(gtable2[['layout']][['name']]=='axis-l')
-  ga <- gtable2[['grobs']][[ia]]
-  ax <- ga[['children']][[2]]
-  ax[['widths']] <- rev(ax[['widths']])
-  ax[['grobs']] <- rev(ax[['grobs']])
-  ax[['grobs']][[1]][['x']] <- ax[['grobs']][[1]][['x']] - unit(1,'npc') + unit(0.15,'cm')
-  graf <- gtable_add_cols(graf, gtable2[['widths']][gtable2[['layout']][ia, ][['l']]], length(graf[['widths']])-1)
-  graf <- gtable_add_grob(graf, ax, par['t'], length(graf[['widths']])-1, par['b'])
-  return(graf)
-}
-
-# Merge plots 1 and 2 for biplot
-biplot <- plot(double_axis_graph(biplot1,biplot2)) # shows 2 y-axes and 1 x-axis
-
 # Bootstrap on eigenvalues and component loadings to determine stability and standard deviations
 library("boot")                       # load the boot package into R
 boot.fn.eig <- function(data, index){ # make a function that returns the 
@@ -141,7 +116,6 @@ eigenvectors3 <- res.prcomp$rotation
 scores3 <- res.prcomp$x
 summary(res.prcomp)
 screeplot(res.prcomp, type="lines")
-biplot(res.prcomp, asp=1)
 
 # Bootstrap via prcomp on eigenvalues and component loadings to determine stability and standard deviations
 boot.prcomp.eig <- function(data, index){   # make a function that returns the 
